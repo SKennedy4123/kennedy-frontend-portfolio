45df1ab9326d103f9ebecfe5518c9a8aee443d1d
SKennedy4123 <148784364+SKennedy4123@users.noreply.github.com>
2024-03-28 11:54:10 -0400

    initial commit, add header and headshot photo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import SarahKennedy from './SarahKennedy.jpg';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={SarahKennedy} className="Headshot" alt="Sarah Kennedy Headshot" />
-        <h1>
-          Sarah Kennedy
-        </h1>
+        <h1>Sarah Kennedy</h1>
+        <h2>Junior Frontend Developer</h2>
+        <em><h4>React • JavaScript • HTML • CSS</h4></em>
       </header>
-    </div>
-  );
-}
+      </div>);}
---
+++ src/Photo.js
@@ -0,0 +1,8 @@
+import React from "react";
+import SarahKennedy from './SarahKennedy.jpg';
+import './Photo.css';
+
+export default function Photo() {
+    return <div><img src={SarahKennedy} alt="Sarah Kennedy Headshot" />
+    </div>
+};
