1d8729e2b3f681fe350753711142df303be93daf
SKennedy4123 <148784364+SKennedy4123@users.noreply.github.com>
2024-03-28 23:14:57 -0400

    Added coder ico, added footer contact links and styled footer.css

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer () {
+    return (
+    <div className="Contact">
+    <ul>
+    <li><strong>Email: </strong><a href="mailto:skennedy4123@gmail.com" rel="noreferrer">SKennedy4123@gmail.com</a></li>
+    <li><strong>LinkedIn: </strong><a href="https://www.linkedin.com/in/sarah-kennedy-02b2b6293/" rel="noreferrer">LinkedIn.com/in/Sarah-Kennedy-02b2b6293/</a></li>
+    <li><strong>GitHub: </strong><a href="https://github.com/SKennedy4123" rel="noreferrer">GitHub.com/SKennedy4123</a></li>
+</ul>
+    <div className="Footer">This page was coded by <a href="https://www.linkedin.com/in/sarah-kennedy-02b2b6293/" target="_blank" rel="noreferrer">Sarah Kennedy</a>, open-sourced on <a href="https://github.com/SKennedy4123" target="_blank" rel="noreferrer">GitHub</a>, and hosted on Netlify.</div>
+    </div>)}
--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import Photo from './Photo';
+import Footer from './Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
     <Photo />
+    <Footer />
   </React.StrictMode>
 );
 
