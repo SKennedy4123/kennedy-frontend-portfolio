3eb78776a61f8cfd5bcc98132dd56d6891ef61b3
SKennedy4123 <148784364+SKennedy4123@users.noreply.github.com>
2024-04-02 20:12:23 -0400

    Add skills and update css

--- src/PhotoBio.js
+++ src/PhotoBio.js
@@ -7,12 +7,13 @@ export default function PhotoBio() {
       <div class="row">
         <div class="col-lg-6">
           <div class="content">
-            <h1 class="mt-5 mb-4">SARAH KENNEDY</h1>
+            <h1 class="mt-5 mb-4" className="Name">SARAH KENNEDY</h1>
             <h2 class="mb-4">Junior Frontend Developer</h2>
-            <h3 class="mb-4">React • Javascript • HTML/CSS</h3>
+            <h3 class="mb-4">React • Javascript • HTML • CSS</h3>
             <p class="mb-5">In addition to being a junior frontend developer, I am an experienced music teacher of 11 years, and a small business owner providing custom designed sugar cookie art. Superb communication, organization, and customer service skills are qualities I bring to any work environment.
             </p>
             <a href="mailto:skennedy4123@gmail.com" type="button" class="btn btn-outline-secondary">Work with me!</a>
+            <p class="mt-5"><strong>Skills: </strong>React.js, Javascript, HTML, CSS, API, SEO, GitHub, Netlify, AI, Git, Bootstrap, Visual Studio Code, Responsive, Hosting, npm</p>
             <h2 class="mt-5">See my work samples:</h2>
             </div>
         </div>
